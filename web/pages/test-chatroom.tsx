39e409a4ac8c785155847b3b5e1143365581b48d
Iverson Diles <ivcotad@gmail.com>
2022-05-20 09:39:37 -0700

    skeleton implementation

--- web/pages/test-chatroom.tsx
+++ web/pages/test-chatroom.tsx
@@ -1,10 +1,11 @@
 import { NextPage } from "next";
 import React from "react";
+import { useForm } from "react-hook-form";
 import {
   useMessagesSubscription,
   useSendMessageMutation,
 } from "../src/generated/graphql";
-import { useForm } from "react-hook-form";
+import styles from "../styles/test-chatroom.module.css";
 
 interface InputInterface {
   username: string;
@@ -13,42 +14,81 @@ interface InputInterface {
 
 const TestChatRoom: NextPage = () => {
   /* 
-Fully working graphql custom subscription hooks
+Fully working example of graphql hooks + react-hook-form
+The basics of the application are being applied on this page.
+
+- Websocket subscription
+- Graphql Mutations
+  - Graphql hooks created with introspection code-gen
+- React-Hook-Form best practices
+
 */
 
-  const { register, handleSubmit, reset } = useForm<InputInterface>();
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    getValues,
+    formState: { errors },
+  } = useForm<InputInterface>();
 
   const { data, loading } = useMessagesSubscription();
-  const [sendMessage, {}] = useSendMessageMutation();
+  const [sendMessage] = useSendMessageMutation();
 
   if (loading) {
-    return <h1>Loading...</h1>;
+    return (
+      <div className={styles.loading}>
+        <h1 className={styles.loading__header}>Loading...</h1>
+      </div>
+    );
   }
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Test Chat Room</h1>
 
       <form
-        onSubmit={handleSubmit(async ({ message }) => {
+        onSubmit={handleSubmit(async ({ username, message }) => {
           await sendMessage({
             variables: {
-              username: "John Doe",
+              username,
               message,
             },
           });
 
-          reset();
+          resetField("message");
         })}
       >
-        <input autoComplete="off" {...register("message")} type="text" />
+        <input
+          placeholder="username"
+          autoComplete="off"
+          {...register("username", { required: "Username is required..." })}
+          type="text"
+        />
+        <input
+          placeholder="message"
+          autoComplete="off"
+          {...register("message", {
+            required: "Message is required...",
+          })}
+          type="text"
+        />
+        <p className={styles.error}>
+          {errors.username?.message} {errors.message?.message}
+        </p>
 
         <input type="submit" />
       </form>
 
       {data?.MessageSubscription?.map((message) => {
         return (
-          <div>
+          <div
+            className={
+              message?.sender === getValues("username")
+                ? `${styles.self} ${styles.message}`
+                : styles.message
+            }
+          >
             <div>
               <b>{message?.sender}:</b> {message?.message}
             </div>
