8030ab91871d529ef07199814ba67a2491b75a35
Iverson Diles <ivcotad@gmail.com>
2022-05-29 08:51:47 -0700

    graphql implementation added to react native app

---
+++ nativeApp/graphql/generated/graphql.tsx
@@ -0,0 +1,94 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Message = {
+  __typename?: 'Message';
+  message?: Maybe<Scalars['String']>;
+  sender?: Maybe<Scalars['String']>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  IsTypingMutation: Typing;
+  SendMessage: Scalars['Boolean'];
+};
+
+
+export type MutationIsTypingMutationArgs = {
+  isTyping: Scalars['Boolean'];
+  username: Scalars['String'];
+};
+
+
+export type MutationSendMessageArgs = {
+  message: Scalars['String'];
+  username: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  ok: Scalars['Boolean'];
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  IsTypingSubscription?: Maybe<Typing>;
+  MessageSubscription?: Maybe<Array<Maybe<Message>>>;
+};
+
+export type Typing = {
+  __typename?: 'Typing';
+  isTyping: Scalars['Boolean'];
+  username: Scalars['String'];
+};
+
+export type MessagesSubscriptionVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MessagesSubscription = { __typename?: 'Subscription', MessageSubscription?: Array<{ __typename?: 'Message', sender?: string | null, message?: string | null } | null> | null };
+
+
+export const MessagesDocument = gql`
+    subscription Messages {
+  MessageSubscription {
+    sender
+    message
+  }
+}
+    `;
+
+/**
+ * __useMessagesSubscription__
+ *
+ * To run a query within a React component, call `useMessagesSubscription` and pass it any options that fit your needs.
+ * When your component renders, `useMessagesSubscription` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the subscription, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useMessagesSubscription({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useMessagesSubscription(baseOptions?: Apollo.SubscriptionHookOptions<MessagesSubscription, MessagesSubscriptionVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useSubscription<MessagesSubscription, MessagesSubscriptionVariables>(MessagesDocument, options);
+      }
+export type MessagesSubscriptionHookResult = ReturnType<typeof useMessagesSubscription>;
+export type MessagesSubscriptionResult = Apollo.SubscriptionResult<MessagesSubscription>;
--- nativeApp/screens/MessageScreen.tsx
+++ nativeApp/screens/MessageScreen.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useMessagesSubscription } from "../graphql/generated/graphql";
 import tw from "../libs/tailwind";
 import { useGlobalStore } from "../libs/useGlobalStore";
 
 const MessageScreen = () => {
   const { username } = useGlobalStore();
+
+  const { loading, data } = useMessagesSubscription();
+
+  console.log(loading);
+
   return (
-    <View style={tw`flex justify-center items-center flex-1`}>
-      <Text style={tw`mt-2 mb-20  text-center`}>
+    <View style={tw`flex items-center justify-center flex-1`}>
+      <Text style={tw`mt-2 mb-20 text-center`}>
         Welcome to the chatroom {username}...{" "}
       </Text>
+      {data?.MessageSubscription?.map((message) => {
+        return (
+          <Text>
+            {" "}
+            {message?.message} {message?.sender}{" "}
+          </Text>
+        );
+      })}
     </View>
   );
 };
