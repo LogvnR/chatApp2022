50e4d7f9456d0e820b78e4a42eeb19d46d3438ed
Iverson Diles <ivcotad@gmail.com>
2022-05-29 09:12:46 -0700

    Added on scroll feature

--- nativeApp/screens/MessageScreen.tsx
+++ nativeApp/screens/MessageScreen.tsx
@@ -1,30 +1,44 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useRef } from "react";
+import { View, Text, ScrollView, ScrollViewProps } from "react-native";
 import { useMessagesSubscription } from "../graphql/generated/graphql";
 import tw from "../libs/tailwind";
 import { useGlobalStore } from "../libs/useGlobalStore";
 
 const MessageScreen = () => {
   const { username } = useGlobalStore();
-
   const { loading, data } = useMessagesSubscription();
-
-  console.log(loading);
+  const scrollViewRef = useRef<ScrollViewProps>();
 
   return (
-    <View style={tw`flex items-center justify-center flex-1`}>
+    <ScrollView
+      style={tw``}
+      // @ts-ignore
+      ref={scrollViewRef}
+      onContentSizeChange={() =>
+        // @ts-ignore
+        scrollViewRef.current.scrollToEnd({ animated: true })
+      }
+    >
       <Text style={tw`mt-2 mb-20 text-center`}>
         Welcome to the chatroom {username}...{" "}
       </Text>
-      {data?.MessageSubscription?.map((message) => {
-        return (
-          <Text>
-            {" "}
-            {message?.message} {message?.sender}{" "}
-          </Text>
-        );
-      })}
-    </View>
+      <View style={tw`flex px-4 pb-10`}>
+        {data?.MessageSubscription?.map((message) => {
+          return (
+            <View
+              style={
+                username === message?.sender
+                  ? tw`self-end max-w-xs p-2 mt-4 bg-blue-400 rounded-md`
+                  : tw`max-w-xs p-2 mt-4 bg-blue-900 rounded-md`
+              }
+            >
+              <Text style={tw`font-bold text-gray-100`}>{message?.sender}</Text>
+              <Text style={tw`mt-1 text-white`}>{message?.message}</Text>
+            </View>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 };
 
