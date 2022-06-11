c0a7a247b6883d8646e4e224ef76034d1ebdab48
Iverson Diles <ivcotad@gmail.com>
2022-06-10 17:53:50 -0700

    workspace & global user

--- web/pages/test-chatroom.tsx
+++ web/pages/test-chatroom.tsx
@@ -1,18 +1,14 @@
-import { NextPage } from 'next';
-import React from 'react';
-
-import { useStore } from '../helpers/useStore';
-
-import { useForm } from 'react-hook-form';
+import { NextPage } from "next";
+import { useForm } from "react-hook-form";
+import { useStore } from "../helpers/useStore";
 import {
   useIsTypingSubscription,
   useMessagesSubscription,
   useSendMessageMutation,
   useSetIsTypingMutation,
-} from '../src/generated/graphql';
+} from "../src/generated/graphql";
 
 interface InputInterface {
-  username: string;
   message: string;
 }
 
@@ -38,7 +34,9 @@ The basics of the application are being applied on this page.
     formState: { errors },
   } = useForm<InputInterface>();
 
-  const { username } = watch();
+  const { username: globalUsername } = useStore();
+
+  // const { username } = watch();
 
   const [sendMessage] = useSendMessageMutation();
   const [setIsTyping] = useSetIsTypingMutation();
@@ -48,7 +46,7 @@ The basics of the application are being applied on this page.
   const isTyping = async (isTyping: boolean) => {
     await setIsTyping({
       variables: {
-        username,
+        username: globalUsername,
         isTyping,
       },
     });
@@ -65,44 +63,33 @@ The basics of the application are being applied on this page.
   return (
     <div className="max-w-3xl px-4 mx-auto ">
       <h1 className="mt-5 text-3xl font-extrabold text-center">
-        Test Chat Room{' '}
+        Test Chat Room{" "}
       </h1>
       <form
         className="flex flex-col items-center gap-2 mt-8 sm:items-start"
-        onSubmit={handleSubmit(async ({ username, message }) => {
+        onSubmit={handleSubmit(async ({ message }) => {
           await sendMessage({
             variables: {
-              username,
+              username: globalUsername,
               message,
             },
           });
 
-          resetField('message');
+          resetField("message");
         })}
       >
-        <input
-          className="w-4/6 px-2 py-1 mx-1 transition-all duration-200 border rounded-sm outline-none sm:w-60 focus-within:ring ring-amber-400"
-          placeholder="username"
-          autoComplete="off"
-          {...register('username', { required: 'Username is required...' })}
-          type="text"
-        />
         <input
           className="w-4/6 px-2 py-1 mx-1 transition-all duration-200 border rounded-sm outline-none sm:w-60 focus-within:ring ring-amber-400"
           placeholder="message"
           autoComplete="off"
           onFocus={async () => isTyping(true)}
-          {...register('message', {
-            required: 'Message is required...',
+          {...register("message", {
+            required: "Message is required...",
             onBlur: async () => isTyping(false),
           })}
           type="text"
         />
-        {errors && (
-          <p className="text-red-500">
-            {errors.username?.message} {errors.message?.message}
-          </p>
-        )}
+        {errors && <p className="text-red-500">{errors.message?.message}</p>}
 
         <button className="bg-[#e3a83a] text-white rounded mx-auto w-32 px-2 py-1 sm:mx-0  hover:bg-[#eebf68] outline-dashed">
           <p> Submit</p>
@@ -115,9 +102,7 @@ The basics of the application are being applied on this page.
             <div
               key={message?.message}
               className={
-                message?.sender === getValues('username')
-                  ? 'place-self-end'
-                  : ''
+                message?.sender === globalUsername ? "place-self-end" : ""
               }
             >
               <div>
